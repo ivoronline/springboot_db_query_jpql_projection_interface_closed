e85408227604388f288509d05aa41dc875f0f47e
ivoronline <ivoronline@gmail.com>
2021-11-26 12:55:09 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_db_query_jpql_projection_interface_closed/entities/PersonProjection.java
+++ src/main/java/com/ivoronline/springboot_db_query_jpql_projection_interface_closed/entities/PersonProjection.java
@@ -1,7 +1,7 @@
 package com.ivoronline.springboot_db_query_jpql_projection_interface_closed.entities;
 
 // Interface can only have Methods. It can't have Properties/State.
-public interface PersonView {
+public interface PersonProjection {
   String getName();
   String getAge();
 }
--- src/main/java/com/ivoronline/springboot_db_query_jpql_projection_interface_closed/repositories/PersonRepository.java
+++ src/main/java/com/ivoronline/springboot_db_query_jpql_projection_interface_closed/repositories/PersonRepository.java
@@ -1,17 +1,17 @@
 package com.ivoronline.springboot_db_query_jpql_projection_interface_closed.repositories;
 
 import com.ivoronline.springboot_db_query_jpql_projection_interface_closed.entities.Person;
-import com.ivoronline.springboot_db_query_jpql_projection_interface_closed.entities.PersonView;
+import com.ivoronline.springboot_db_query_jpql_projection_interface_closed.entities.PersonProjection;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PersonRepository extends CrudRepository<Person, Integer> {
 
   //=======================================================================================
-  // RETURN PERSON VIEW
+  // RETURN PERSON PROJECTION
   //=======================================================================================
   @Query(value = "SELECT person.name AS name, person.age AS age FROM Person person WHERE person.name='John'")
-  PersonView returnPersonView();
+  PersonProjection returnPersonProjection();
 
 }
 
